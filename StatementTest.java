7e31fb02d3344334481b675ca9be7eb9baf9c597
harledge <42972091+harledge@users.noreply.github.com>
2018-10-30 23:51:07 -0400

    Update StatementTest.java

--- StatementTest.java
+++ StatementTest.java
@@ -14,7 +14,7 @@ import components.utilities.Tokenizer;
  * JUnit test fixture for {@code Statement}'s constructor and kernel methods.
  *
  * @author Wayne Heym
- * @author Put your name here
+ * @author Nik Anand and Hudson Arledge
  *
  */
 public abstract class StatementTest {
